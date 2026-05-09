10fc15cb3eb4f77dc97e38477ceeb747ff341274
Nagaratna Naik <nagaratnanaik955@gmail.com>
2026-05-09 15:10:48 +0530

    Update ALLOWED_HOSTS for Vercel

--- meal_buddy_project/settings.py
+++ meal_buddy_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ibgxgz%ry$rcf=!8rqzbz&1=nqy5ux=qrnkp2u3gu-_k$5bfbx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
